e727b13c615f2dc1281718c62772d6cd3f8f063a
ssr0016 <samsonrecluta0016@gmail.com>
2024-10-21 23:29:00 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+const portNumber = ":4000"
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.page.tmpl")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.page.tmpl")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parseTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parseTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template:", err)
+		return
+	}
+}
+
+func main() {
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
+}
